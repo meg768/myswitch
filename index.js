d34bb031a6d793787197c8e6b12faf9ead7cc8e8
meg768 <meg768@users.noreply.github.com>
2017-12-05 15:48:52 +0100

    -

--- index.js
+++ index.js
@@ -23,11 +23,29 @@ mySwitch.prototype = {
         state = on;
         debug('State is now', state);
         return next();
+    },
+    getServices: function() {
+        let informationService = new Service.AccessoryInformation();
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
+            .setCharacteristic(Characteristic.Model, "My switch model")
+            .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+        let switchService = new Service.Switch("My switch");
+        switchService
+            .getCharacteristic(Characteristic.On)
+            .on('get', this.getSwitchOnCharacteristic.bind(this))
+            .on('set', this.setSwitchOnCharacteristic.bind(this));
+
+        this.informationService = informationService;
+        this.switchService = switchService;
+        return [informationService, switchService];
     }
+
 };
 
-module.exports = function (homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch);
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch);
 };
